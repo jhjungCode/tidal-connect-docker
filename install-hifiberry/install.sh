1ec2323caca4dc4557b3a8d927da16303c88206d
JongHwa Jung <jhjung0203@gmail.com>
2021-07-09 23:46:02 +0900

    Update install.sh
    fixed symbolic link error

--- install-hifiberry/install.sh
+++ install-hifiberry/install.sh
@@ -7,7 +7,7 @@ systemctl enable tidal.service
 
 # Add Tidal Source to Beocreate
 printf  "\nAdding Tidal Source to Beocreate UI"
-ln -s /opt/beocreate/beo-extensions/tidal ${PWD}/beocreate/beo-extensions/tidal
+ln -s ${PWD}/beocreate/beo-extensions/tidal /opt/beocreate/beo-extensions/tidal
 
 printf "\nStarting Tidal Connect Service..."
 systemctl start tidal.service
